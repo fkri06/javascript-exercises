2fdaca549a3ee35d021cd5371cb7b35700cf3c39
Fikri Aqsha <m.fikriaqsha@gmail.com>
2026-08-31 19:05:05 +0700

    Finish data types and conditionals exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let repeatStr = "";
+    for (let i = 0; i < num; i++) {
+        repeatStr += string;
+    }
+    return repeatStr;
 };
 
 // Do not edit below this line
