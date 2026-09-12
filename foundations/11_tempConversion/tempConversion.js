81496faf678dc3b250a39f7eefc585d07d387045
Fikri Aqsha <m.fikriaqsha@gmail.com>
2026-09-12 19:01:52 +0700

    Finish Loops and Arrays exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let result = (num - 32) * (5 / 9);
+  return Math.round(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let result = (num * (9 / 5)) + 32;
+  return Math.round(result);
 };
 
 // Do not edit below this line
