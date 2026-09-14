b435e140684c0a89623aa7b24cbcc6d3be8ef9b6
Fikri Aqsha <m.fikriaqsha@gmail.com>
2026-09-14 19:17:34 +0700

    finish assignment 14 fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof num == "string") {
+        num = Number.parseInt(num);
+    }
 
+    if (num == 0) {
+        return 0;
+    }
+
+    if (num == 1) {
+        return 1;
+    }
+    
+    if (num < 0) {
+        return "OOPS";
+    }
+
+    let fib = {
+        current: 0,
+        result: 1,
+    }
+
+    for (let i = 0; i < num - 1; i++) {
+        const result = fib.current + fib.result;
+        fib.current = fib.result;
+        fib.result = result;
+    }
+
+    return fib.result;
 };
 
 // Do not edit below this line
