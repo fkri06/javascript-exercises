f35880ad630aba057c8a931cb6addf47eeccdae3
Fikri Aqsha <m.fikriaqsha@gmail.com>
2026-09-14 18:20:34 +0700

    Finish assignment 13 palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let isPalindrome = true;
 
+    string = string.toLowerCase();
+
+    if (string.includes(" ")) {
+        string = string.split(" ").join("");
+    }
+
+    if (string.includes(",")) {
+        string = string.split(",").join("");
+    }
+
+    if (string.includes("!")) {
+        string = string.split("!").join("");
+    }
+
+    if (string[string.length - 1] === ".") {
+        string = string.replace(".", "").split(",").join("").split(" ").join("");
+    }
+
+    for (let i = 0; i < Math.round(string.length / 2); i++) {
+        if (string[i] !== string[string.length - 1 - i]) {
+            isPalindrome = false;
+        }
+    }
+
+    return isPalindrome;
 };
 
 // Do not edit below this line
