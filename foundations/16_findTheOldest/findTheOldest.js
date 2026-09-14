04d1734d746d5addf4700074dbf62bdf4d024964
Fikri Aqsha <m.fikriaqsha@gmail.com>
2026-09-15 05:49:18 +0700

    Finish exercise 16 find the oldest person

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const newPeople = people.map((people) => {
+        let yearsLived;
+        if ("yearOfDeath" in people) {
+            yearsLived = people.yearOfDeath - people.yearOfBirth;
+        } else {
+            yearsLived = (new Date().getFullYear()) - people.yearOfBirth;
+        }
+        return {name: people.name, lifespan: yearsLived}
+    });
 
+    return newPeople.sort((a, b) => b.lifespan - a.lifespan)[0];
 };
 
 // Do not edit below this line
